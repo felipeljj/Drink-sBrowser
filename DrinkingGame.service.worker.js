6cb9e412aaa9d35e5d27a58c55c36ca5eaeb744e
felipeljj <felipemartins@trabalhabrasil.com.br>
2025-12-05 16:29:34 -0300

    Patch day 1 - 1.0.0

--- DrinkingGame.service.worker.js
+++ DrinkingGame.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1764962928|13202265086';
+const CACHE_VERSION = '1764962965|13239815951';
 /** @type {string} */
 const CACHE_PREFIX = 'DrinkingGame-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
